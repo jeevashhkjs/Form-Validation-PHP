d066a2a342a325a029a91f7bfb2116da043a573a
jeevashhkjs <jeevasdckap@gmail.com>
2023-03-22 13:00:19 +0530

    log work properly

--- log.php
+++ log.php
@@ -6,17 +6,17 @@
  echo  $pasd;
 
  try{
-    $check = $conn -> prepare("SELECT * FROM employees where username =  $user and passwod = $pasd ");
+    $check = $conn -> prepare("SELECT * FROM employees where username =  '$user' and passwod = '$pasd' ");
     $check -> execute();
 
     $fetchAllDatas = $check -> fetchAll(PDO :: FETCH_OBJ);
 
-    print_r($fetchAllDatas);
-    // if($check){
-    //     echo "You have id";
-    // }else{
-    //     echo "Sryy you have no id";
-    // }
+   if($fetchAllDatas == []){
+      echo "you have no account";
+   }
+   else{
+      echo "you have a account";
+   }
  }
  catch(Exception $e){
     echo "no user".$e->getMessage();
